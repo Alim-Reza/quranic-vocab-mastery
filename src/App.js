c7b8f9e5e7a58bb6b59b9aec1326e76f6548a336
Alim-Reza <aarasif2@gmail.com>
2023-04-17 16:28:23 +0600

    basic frame done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,175 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  const arabicText = "مرحبا بالعالم"; // Replace with your Arabic text
   return (
     <div className="App">
-      <div dir="rtl" className="card">
-        {arabicText}
-      </div>
-      <div className="container">
-        <div className="item">yo</div>
-        <div className="item">loren ipsum</div>
-        <div className="item">loren ipsum and something</div>
-        <div className="item">yo.....</div>
+      <Frame
+        arabicText="مرحبا بالعالم"
+        listOfOptions={['sd', 'asdfasd', 'asdfas', 'asdasas']}
+      />
+      <div style={{ paddingTop: '2em' }}>
+        <button
+          style={{
+            background: '#00acc1',
+            border: '1px solid #00acc1',
+            borderRadius: '110px',
+            height: '2em',
+            width: '10em',
+          }}
+        >
+          next
+        </button>
       </div>
     </div>
   );
 }
-
+const Frame = ({ arabicText, listOfOptions }) => (
+  <div>
+    <div dir="rtl" className="card">
+      {arabicText}
+    </div>
+    <div className="container">
+      {listOfOptions.map((item, index) => (
+        <div className="item" key={index}>
+          {item}
+        </div>
+      ))}
+    </div>
+  </div>
+);
 export default App;
+const data_set = [
+  {
+    ayahNumber: 0,
+    phraseEnglish: 'Bismillah',
+    phraseArabic: 'بِسْمِ اللهِ ',
+    meaning: 'In the name of Allah',
+  },
+  {
+    ayahNumber: 0,
+    phraseEnglish: 'Ar-Rahman',
+    phraseArabic: 'الرَّحْمٰنِ ',
+    meaning: 'The Most Gracious',
+  },
+  {
+    ayahNumber: 0,
+    phraseEnglish: 'Ar-Rahim',
+    phraseArabic: 'الرَّحِيْمِ ',
+    meaning: 'The Most Merciful',
+  },
+  {
+    ayahNumber: 1,
+    phraseEnglish: 'Al-hamdu',
+    phraseArabic: 'الْحَمْدُ ',
+    meaning: 'All praise',
+  },
+  {
+    ayahNumber: 1,
+    phraseEnglish: 'Lillahi',
+    phraseArabic: 'لِلَّهِ ',
+    meaning: 'is for Allah',
+  },
+  {
+    ayahNumber: 1,
+    phraseEnglish: 'Rabb',
+    phraseArabic: 'رَبِّ ',
+    meaning: 'the Lord',
+  },
+  {
+    ayahNumber: 1,
+    phraseEnglish: 'Alamin',
+    phraseArabic: 'الْعَالَمِيْنَ ',
+    meaning: 'of the worlds',
+  },
+  {
+    ayahNumber: 3,
+    phraseEnglish: 'Maliki',
+    phraseArabic: 'مَالِكِ ',
+    meaning: 'Owner',
+  },
+  {
+    ayahNumber: 3,
+    phraseEnglish: 'Yawm',
+    phraseArabic: 'يَوْمِ ',
+    meaning: 'of the Day',
+  },
+  {
+    ayahNumber: 3,
+    phraseEnglish: 'Ad-Din',
+    phraseArabic: 'الدِّيْنِ ',
+    meaning: 'of Judgement',
+  },
+  {
+    ayahNumber: 4,
+    phraseEnglish: 'Iyyaka',
+    phraseArabic: 'إِيَّاكَ ',
+    meaning: 'You alone',
+  },
+  {
+    ayahNumber: 4,
+    phraseEnglish: "Na'budu",
+    phraseArabic: 'نَعْبُدُ ',
+    meaning: 'we worship',
+  },
+  {
+    ayahNumber: 4,
+    phraseEnglish: 'Wa-iyyaka',
+    phraseArabic: 'وَإِيَّاكَ ',
+    meaning: 'and You alone',
+  },
+  {
+    ayahNumber: 4,
+    phraseEnglish: "Nasta'in",
+    phraseArabic: 'نَسْتَعِيْنُ ',
+    meaning: 'we seek help',
+  },
+  {
+    ayahNumber: 5,
+    phraseEnglish: 'Ihdina',
+    phraseArabic: 'اهْدِنَا ',
+    meaning: 'Guide us',
+  },
+  {
+    ayahNumber: 5,
+    phraseEnglish: 'As-sirat',
+    phraseArabic: 'الصِّرَاطَ ',
+    meaning: 'the Straight Path',
+  },
+  {
+    ayahNumber: 5,
+    phraseEnglish: 'Al-Mustaqim',
+    phraseArabic: 'الْمُسْتَقِيْمَ ',
+    meaning: 'the path of those whom You have favored',
+  },
+  {
+    ayahNumber: 6,
+    phraseEnglish: 'Sirat',
+    phraseArabic: 'صِرَاطَ ',
+    meaning: 'not the path',
+  },
+  {
+    ayahNumber: 6,
+    phraseEnglish: 'Al-Ladhina',
+    phraseArabic: 'الَّذِينَ ',
+    meaning: 'of those who',
+  },
+  {
+    ayahNumber: 6,
+    phraseEnglish: "An'amta",
+    phraseArabic: 'أَنْعَمْتَ ',
+    meaning: 'have earned Your anger',
+  },
+  {
+    ayahNumber: 6,
+    phraseEnglish: 'Alayhim',
+    phraseArabic: 'عَلَيْهِمْ ',
+    meaning: 'upon them',
+  },
+  {
+    ayahNumber: 7,
+    phraseEnglish: 'Wa-lad-dallin',
+    phraseArabic: 'وَلاَ الضَّالِّيْنَ ',
+    meaning: 'nor of those who go astray.',
+  },
+];
