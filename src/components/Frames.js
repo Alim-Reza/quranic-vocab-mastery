f6e5ce6efbf1063e7d45aa46f00ac8727b0ad72d
Alim-Reza <aarasif2@gmail.com>
2023-04-17 23:12:17 +0600

    1 file modified

--- src/components/Frames.js
+++ src/components/Frames.js
@@ -9,11 +9,18 @@ export default function Frame({ arabicText, listOfOptions }) {
       </div>
       <div className="container">
         {listOfOptions.map((item, index) => (
-          <div className="item" key={index}>
-            {item}
-          </div>
+          <FrameItem item={item} index={index} />
         ))}
       </div>
     </div>
   );
 }
+
+const FrameItem = ({ item, index }) => {
+  const [buttonBg, setButtonBg] = React.useState('transparent');
+  return (
+    <div className="item" key={index} style={{ background: buttonBg }}>
+      {item}
+    </div>
+  );
+};
